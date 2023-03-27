c1b41fc84c98eaa2f0bf19dcc583bb1bad2d4b45
Christian Cabrera <ccabrera.cd@gmail.com>
2023-03-27 02:50:57 -0300

    arreglando responive general

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -18,19 +18,19 @@ const MenuNav = () => {
 
   const handleScroll = () => {
     if (window.scrollY > 100) {
-      setIsScrolled(true);
+      setIsScrolled(true)
     } else {
-      setIsScrolled(false);
+      setIsScrolled(false)
     }
   };
 
 
   return (
     <>
-    <nav className={`navbar navbar-expand-lg navbar-dark move fixed-top ${isScrolled ? 'bg-dark' : ''}`}>
+    <nav className={`navbar navbar-expand-lg navbar-dark move fixed-top ${window.innerWidth < 957? 'bg-dark':''} ${isScrolled ? 'bg-dark' : ''}`}>
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          <img src={logo} alt="Divi Logo" className={`logo ${isScrolled ? 'consize' : ''}`} />
+          <img src={logo} alt="Divi Logo" className={`logo ${window.innerWidth < 957? 'consize' : ''} ${isScrolled ? 'consize' : ''}`} />
         </a>
          <ul className={`navbar-nav navbar-toggler ${isScrolled ? '' : 'd-none'}`}>
             <li className="nav-item ">
@@ -82,32 +82,3 @@ const MenuNav = () => {
 export default MenuNav
 
 
-
-{/* <nav className="navbar navbar-expand navbar-light bg-light px-5">
-  <NavLink className="navbar-brand" to='/'>LaLocura</NavLink>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <div className="nav-link"><NavLink className="nav-link nav-link" to='/'>Inicio<span className="sr-only"></span></NavLink></div>
-      </li>
-      <li className="nav-item">
-      <div className="nav-link text-dark">
-        <NavLink to='/Contacto' className="nav-link">Contacto</NavLink></div>
-      </li>
-      <li className="nav-item">
-      <div className="nav-link">
-        <NavLink className="nav-link" to='/categoria/remera'>Remeras</NavLink>
-      </div>
-      </li>
-      <li className="nav-item">
-      <div className="nav-link"><NavLink className="nav-link" to='/categoria/chomba'>Chombas</NavLink></div>
-      </li>
-      <li className="nav-item">
-      <div className="nav-link"><NavLink className="nav-link" to='/cart'><CartWidget/></NavLink></div>
-      </li>
-    </ul>
-  </div>
-</nav> */}
