aef32896eba8388a18d3cc716c1accc45d4cde3e
Christian Cabrera <ccabrera.cd@gmail.com>
2023-03-28 20:09:48 -0300

    orden productos y filtro oferta

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -2,7 +2,8 @@ import React, {useState, useEffect} from 'react'
 import {getFirestore,collection,getDocs,query,where} from 'firebase/firestore'
 import Title from '../Title';
 import ItemList from '../ItemList';
-import {useParams} from 'react-router-dom'
+import {Link,useParams} from 'react-router-dom'
+import Item from '../Item';
 
 
 
@@ -14,28 +15,71 @@ const ItemListContainer = (props) => {
   useEffect(()=> {
 
     const querydb = getFirestore()
-        const queryCollection = collection(querydb, 'products')
-    /* const getData=new Promise(resolve=>{
-      setTimeout(()=>{
-        resolve(productos)
-      },500)
-    }); */
+    const queryCollection = collection(querydb, 'products')
+
      if(categoriaId) {
+      if (categoriaId==="oferta") {
+        const queryFilter= query(queryCollection, where('oferta','>',0))
+        getDocs(queryFilter)
+          .then(res => setData(res.docs.map(product=> ({id: product.id, ...product.data()}))))
+      } else {
       const queryFilter= query(queryCollection, where('categoria','==',categoriaId))
       getDocs(queryFilter)
         .then(res => setData(res.docs.map(product=> ({id: product.id, ...product.data()}))))
+      }
       } else {
         getDocs(queryCollection)
-        .then(res => setData(res.docs.map(product=> ({id: product.id, ...product.data()}))))
+         .then(res => setData(res.docs.map(product=> ({id: product.id, ...product.data()}))))
       }
   }, [categoriaId])
 
+ const select  = (item) => {
+  if (item==="mayor")  {
+    setData([...data].sort((a, b) => b.precio - a.precio));
+  } else {
+    setData([...data].sort((a, b) => a.precio - b.precio));
+  }
+ }
 
 
   return (
+
     <>
-    {/* <Title greeting="Bienvenido!"/> */}
+
+    {categoriaId? '':<Title greeting="Bienvenido!"/>}
+
+    <header class="p-3 text-bg-dark mt-2">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><Link class="nav-link px-2 text-white" to="/">TODOS</Link></li>
+          <li><Link class="nav-link px-2 text-white" to="/categoria/remera">Remeras</Link></li>
+          <li><Link class="nav-link px-2 text-white" to="/categoria/chomba">Chombas</Link></li>
+          <li><p class="nav-link px-2  text-secondary">Ordenar de: </p></li>
+          <li><button class="nav-link px-2 text-white" onClick={()=>{select("mayor")}}>Mayor - menor</button></li>
+          <li><button class="nav-link px-2 text-white" onClick={()=>{select("menor")}}>Menor - mayor</button></li>
+        </ul>
+    
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+          
+          <input type="text" class="form-control form-control-dark text-bg-dark d-none"
+           aria-label="Search" spellcheck="false" data-ms-editor="true"/>
+        </form>
+
+        <div class="text-end">
+          <Link type="button" to="/categoria/oferta" class="btn btn-warning">Ofertas</Link>
+        </div>
+      </div>
+    </div>
+  </header>
+
     <ItemList data={data}/>
+
+
+    
+
+
     </>
   )
 }
--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
-
-const index = (props) => {
+import Carousel from 'react-bootstrap/Carousel';
+import './style.css'
+function Title() {
   return (
-    <h1 class="mt-2 px-4">{props.greeting}</h1>
-  )
+    <>
+    <div className='container mt-5 scale'>
+    <Carousel>
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          src="https://i.ibb.co/f4VnGFw/bd1411ac80089ea07d495f277e160131.jpg"
+          alt="Abran Paso, tenemos las mejores promos"
+        />
+        <Carousel.Caption>
+          <h3>Tenemos las mejores promos!</h3>
+          <p>Sabemos lo que hacemos, por eso te damos ofertas para que disfrutes nuestras remeras.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          className="d-block w-100"
+          src="https://i.ibb.co/d0jWNyh/3.jpg"
+          alt="anda pa alla"
+        />
+        <Carousel.Caption>
+          <h3>Que miras?? </h3>
+          <p>Anda pa lla...</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.ibb.co/Lxgb2Sv/2.jpg"
+          alt="2023"
+        />
+        <Carousel.Caption>
+          <h3>Comenzamos con todo! </h3>
+          <p>
+            Bienvenido 2023.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
+    </>
+  );
 }
 
-export default index
+export default Title;
